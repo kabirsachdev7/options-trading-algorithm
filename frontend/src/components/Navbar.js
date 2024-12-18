301331f4a3e33b9ae18ede44acf82fa9e08faadf
evanykauh <evan.kauh@gmail.com>
2024-12-17 20:57:43 -0500

    Login / Registration

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 const NavigationBar = () => {
   const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -18,17 +19,31 @@ const NavigationBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Dashboard
-            </Nav.Link>
-            <Nav.Link as={Link} to="/portfolio">
-              Portfolio
-            </Nav.Link>
-            {/* Add more links as needed */}
+        <Nav className="me-auto">
+            {token && (
+              <>
+                <Nav.Link as={Link} to="/">
+                  Dashboard
+                </Nav.Link>
+                <Nav.Link as={Link} to="/portfolio">
+                  Portfolio
+                </Nav.Link>
+              </>
+            )}
           </Nav>
           <Nav>
-            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            {token ? (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
